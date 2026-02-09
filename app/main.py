11863a4f475dd5ddb46938dd95984344c860c3ab
Karol <karolmarkut11@gmail.com>
2026-02-09 08:01:46 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -2,4 +2,34 @@ class Cargo:
     def __init__(self, weight: int) -> None:
         self.weight = weight
 
-# write your code here
+class BaseRobot:
+    def __init__(self, name: str, weight: int, coords: tuple(int, int)) -> None:
+        self.name = name
+        self.weight = weight
+        self.coords = coords
+
+    def move (self) -> None:
+        if self.coords = go_forward:
+            coords.y += 1
+        elif self.coords = go_back:
+            coords.y -= 1
+        elif self.coords = go_right:
+            coords.x += 1
+        elif self.coords = go_left:
+            coords.x -= 1
+        else:
+            print("Wrong instruction")
+    def get_info(self) -> str:
+        return f"Robot: {name}, Weight: {weight}"
+
+class FlyingRobot(BaseRobot):
+    def __init__(self, name: str, weight: int, coords: tuple(int, int, int)) -> None:
+        super().__init__(name, weight, coords)
+        self.coords = [0,0,0]
+
+    def move(self) -> None:
+        if self.coords = go_up:
+            coords.z += 1
+        elif self.coords = go_down:
+            coords.z -= 1
+
